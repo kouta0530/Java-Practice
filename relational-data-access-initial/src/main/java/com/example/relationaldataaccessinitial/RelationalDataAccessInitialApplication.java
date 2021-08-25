bc5a0d9d6cd8cdc10eb91b9ed882e7711a546ee1
kouta0530 <16jk047@ymklab.net>
2021-08-25 17:14:56 +0900

    feat(relational): spring initializrで環境構築

---
+++ relational-data-access-initial/src/main/java/com/example/relationaldataaccessinitial/RelationalDataAccessInitialApplication.java
@@ -0,0 +1,13 @@
+package com.example.relationaldataaccessinitial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalDataAccessInitialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationalDataAccessInitialApplication.class, args);
+	}
+
+}
