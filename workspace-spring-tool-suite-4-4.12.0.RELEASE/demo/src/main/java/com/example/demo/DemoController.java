b85215fcd5cc9d0790f8c070797f58fd2dca3e1c
kouta0530 <16jk047@ymklab.net>
2021-10-26 21:16:00 +0900

    fix(controller): 文字列からhtmlファイルの指定に変更する

--- workspace-spring-tool-suite-4-4.12.0.RELEASE/demo/src/main/java/com/example/demo/DemoController.java
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/demo/src/main/java/com/example/demo/DemoController.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class DemoController {
 	@RequestMapping("/")
-	String home() {
-		return "Hello Spring Boot !";
+	String index() {
+		return "index";
 	}
 }
