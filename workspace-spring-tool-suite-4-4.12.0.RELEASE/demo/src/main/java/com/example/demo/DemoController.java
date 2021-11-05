856156e0e94eaa77b1a39a9c69e0d345131e4182
kouta0530 <16jk047@ymklab.net>
2021-11-05 23:41:08 +0900

    fix(controller): GETとPOSTのマッピングオブジェクトをimportして同一の結果を得る

--- workspace-spring-tool-suite-4-4.12.0.RELEASE/demo/src/main/java/com/example/demo/DemoController.java
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/demo/src/main/java/com/example/demo/DemoController.java
@@ -1,21 +1,21 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DemoController {
-	@RequestMapping(value="/",method=RequestMethod.GET)
+	@GetMapping("/")
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
 		mav.addObject("msg","お名前を入力してください");
 		return mav;
 	}
 	
-	@RequestMapping(value="/",method=RequestMethod.POST)
+	@PostMapping(value="/")
 	public ModelAndView send(
 			@RequestParam("text1")String str,
 			ModelAndView mav
