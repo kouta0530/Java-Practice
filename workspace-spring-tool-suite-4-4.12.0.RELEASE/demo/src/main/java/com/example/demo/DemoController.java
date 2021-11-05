e035893a46c17f93e77c77f779fac3bc68f01bcb
kouta0530 <16jk047@ymklab.net>
2021-11-05 23:27:48 +0900

    fix(controller): modelAndViewを使ってコントローラーとビューを連携する

--- workspace-spring-tool-suite-4-4.12.0.RELEASE/demo/src/main/java/com/example/demo/DemoController.java
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/demo/src/main/java/com/example/demo/DemoController.java
@@ -3,17 +3,18 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DemoController {
 	@RequestMapping("/{num}")
-	String index(@PathVariable int num, Model model) {
+	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
 		int sum = 0;
 		for(int i= 1; i<= num; i++) {
 			sum += i;
 		}
-		model.addAttribute("msg","sum=" + sum);
-		return "index";
+		mav.addObject("msg","sum=" + sum);
+		mav.setViewName("index");
+		return mav;
 	}
 }
