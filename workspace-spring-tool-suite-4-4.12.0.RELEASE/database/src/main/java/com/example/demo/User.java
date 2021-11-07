79d319a6a23af5d0ed7d8e5a3fa8304ab0088554
kouta0530 <16jk047@ymklab.net>
2021-11-07 18:34:18 +0900

    feat(knocks):　データベースに対応するために、エンティティにアノテーションを設ける

--- workspace-spring-tool-suite-4-4.12.0.RELEASE/database/src/main/java/com/example/demo/User.java
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/database/src/main/java/com/example/demo/User.java
@@ -1,7 +1,20 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@Table(name="user")
 public class User {
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	@Column
 	private int id;
+	@Column
 	private String name;
 	public int getId() {
 		return id;
