628e88cfff74160b3b2de22a101978a1642cbe72
kouta0530 <16jk047@ymklab.net>
2021-11-13 13:02:43 +0900

    feat(controller): トップ画面と初期データ作成用のコントローラーを用意する

---
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/main/java/com/example/demo/BoardController.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.BoardRepository;
+import javax.annotation.PostConstruct;
+
+import java.util.List;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+@Controller
+public class BoardController {
+
+	@Autowired
+	BoardRepository repos;
+	
+	@GetMapping
+	public ModelAndView list() {
+		ModelAndView mav = new ModelAndView();
+		List<Board> list = repos.findAll();
+		
+		mav.setViewName("board/list");
+		mav.addObject("data",list);
+		return mav;
+	}
+	
+	@PostConstruct 
+	public void init() {
+		Board board1 = new Board();
+		board1.setTitle("帰社日について");
+		board1.setUpsert(new Date());
+		board1.setWriter("島根");
+		board1.setContent("帰社日は以下の通りに決定しました。2018/01/11");
+		repos.saveAndFlush(board1);
+		
+		Board board2 = new Board();
+		board2.setTitle("新入社員歓迎会のおしらせ");
+		board2.setUpsert(new Date());
+		board2.setWriter("島根");
+		board2.setContent("");
+		repos.saveAndFlush(board2);
+	}
+}
