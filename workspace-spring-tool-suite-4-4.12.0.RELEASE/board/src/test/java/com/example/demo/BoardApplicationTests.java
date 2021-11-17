fcfaa3ad333893af32504e5b1557d85b19b5134e
kouta0530 <16jk047@ymklab.net>
2021-11-17 21:35:56 +0900

    feat(test): アプリケーションの起動テストを作成

--- workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/test/java/com/example/demo/BoardApplicationTests.java
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/test/java/com/example/demo/BoardApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.BoardController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BoardApplicationTests {
-
+	
+	@Autowired
+	BoardController controller;
+	
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
