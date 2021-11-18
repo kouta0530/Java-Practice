b6a2e5335a0167ae8e0cf99a26c2c047e068c478
kouta0530 <16jk047@ymklab.net>
2021-11-18 23:37:23 +0900

    feat(test): controllerのトップページのリクエスト確認テストの作成

---
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/test/java/com/example/demo/BoardControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import com.example.demo.BoardController;
+
+@WebMvcTest(controllers=BoardController.class)
+public class BoardControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void returnMessageFromBoardController() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+	}
+
+}
