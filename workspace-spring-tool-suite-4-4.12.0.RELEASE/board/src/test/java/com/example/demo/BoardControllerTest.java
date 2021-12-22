cd215b08b8b16be029c9cf9a473ccae95fa9a9a3
kouta0530 <16jk047@ymklab.net>
2021-12-22 23:56:41 +0900

    feat(test): detailページのリクエストの確認用のテストを追加する

--- workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/test/java/com/example/demo/BoardControllerTest.java
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/test/java/com/example/demo/BoardControllerTest.java
@@ -9,19 +9,26 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import com.example.demo.BoardController;
 
-@WebMvcTest(controllers=BoardController.class)
+@WebMvcTest(controllers = BoardController.class)
 public class BoardControllerTest {
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Test
 	public void returnMessageFromBoardController() throws Exception {
 		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+	}
+
+	@Test
+	public void getRequestFromDetailDomain() throws Exception {
+		this.mockMvc.perform(get("/detail")).andDo(print()).andExpect(status().isOk());
 	}
 
 }
