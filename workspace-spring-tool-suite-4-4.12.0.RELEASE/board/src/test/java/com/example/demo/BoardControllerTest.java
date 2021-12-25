dcf0e4e44e1cd418c022ae4533931f55cce59dda
kouta0530 <16jk047@ymklab.net>
2021-12-25 23:48:17 +0900

    fix(test): 不要なインストールを削除する

--- workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/test/java/com/example/demo/BoardControllerTest.java
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/test/java/com/example/demo/BoardControllerTest.java
@@ -1,18 +1,13 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.concurrent.ExecutionException;
-
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
 import com.example.demo.BoardController;
 
 @WebMvcTest(controllers = BoardController.class)
@@ -25,10 +20,4 @@ public class BoardControllerTest {
 	public void returnMessageFromBoardController() throws Exception {
 		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
 	}
-
-	@Test
-	public void getRequestFromDetailDomain() throws Exception {
-		this.mockMvc.perform(get("/detail")).andDo(print()).andExpect(status().isOk());
-	}
-
 }
