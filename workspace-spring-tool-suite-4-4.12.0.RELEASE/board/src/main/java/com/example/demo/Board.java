d37a89e14b45d9e4fc1d6eb9acb4957597d55da3
kouta0530 <16jk047@ymklab.net>
2022-01-05 14:51:51 +0900

    feat(model): bean-validationを使ってモデルのカラムのバリデーションを行う

--- workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/main/java/com/example/demo/Board.java
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/main/java/com/example/demo/Board.java
@@ -10,48 +10,64 @@ import javax.persistence.GenerationType;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Entity
-@Table(name="board")
+@Table(name = "board")
 public class Board {
 	@Id
-	@GeneratedValue(strategy= GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column
 	private int id;
 	@Column
 	private Date upsert;
 	@Column
+	@NotEmpty(message="タイトルを入力してください")
+	@Size(max=255,message="255文字以内で入力してください")
 	private String title;
 	@Column
+	@NotEmpty(message="本文を記載してください")
 	private String content;
 	@Column
 	private String writer;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public Date getUpsert() {
 		return upsert;
 	}
+
 	public void setUpsert(Date upsert) {
 		this.upsert = upsert;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public String getWriter() {
 		return writer;
 	}
+
 	public void setWriter(String writer) {
 		this.writer = writer;
 	}
