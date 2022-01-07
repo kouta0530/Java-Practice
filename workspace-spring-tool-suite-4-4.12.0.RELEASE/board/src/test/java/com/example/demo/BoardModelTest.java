8ba502ef8f08808f923107a3ddd8df1f05d82fe9
kouta0530 <16jk047@ymklab.net>
2022-01-07 20:57:08 +0900

    feat(test): 最大文字数を越えている場合のバリデーションエラーが適切に機能するかテストする

--- workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/test/java/com/example/demo/BoardModelTest.java
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/test/java/com/example/demo/BoardModelTest.java
@@ -6,10 +6,12 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.util.StringUtils;
 
 @SpringBootTest
 public class BoardModelTest {
@@ -22,5 +24,13 @@ public class BoardModelTest {
     Board board = new Board();
     Set<ConstraintViolation<Board>> violations = this.validator.validateProperty(board, "title");
     assertThat(violations).hasSize(1);
+  }
+
+  @Test
+  public void BoardTitleMoreThanMaxSize() {
+    Board board = new Board();
+    board.setTitle(StringUtils.repeat("a", 256));
+    Set<ConstraintViolation<Board>> violations = this.validator.validateProperty(board, "title");
+    assertEquals("255文字以内で入力してください", violations.iterator().next().getMessage());
   }
 }
