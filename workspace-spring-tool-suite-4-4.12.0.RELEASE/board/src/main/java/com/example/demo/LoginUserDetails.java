6d91e2e15bb3ebac31752f54f8ef8987199bfe78
kouta0530 <16jk047@ymklab.net>
2022-01-18 23:30:51 +0900

    feat(model): userモデルとspring.securityのuserを紐付ける

---
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/main/java/com/example/demo/LoginUserDetails.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+
+public class LoginUserDetails extends org.springframework.security.core.userdetails.User {
+	private static final long serialVersionUID = 1L;
+	private final User user;
+	
+	public LoginUserDetails(User user) {
+		super(user.getUsername(),user.getEncodedPassword(),AuthorityUtils.createAuthorityList("ROLE USER"));
+		this.user = user;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	
+}
