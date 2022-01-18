a7f81d5c69145c52a9f64d58af39715d7d1ba22a
kouta0530 <16jk047@ymklab.net>
2022-01-18 23:29:35 +0900

    feat(service): ログインユーザーのサービス部分を作成する

---
+++ workspace-spring-tool-suite-4-4.12.0.RELEASE/board/src/main/java/com/example/demo/LoginUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginUserDetailsService implements UserDetailsService {
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public LoginUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("user not found");
+		}
+
+		return new LoginUserDetails(user);
+	}
+}
